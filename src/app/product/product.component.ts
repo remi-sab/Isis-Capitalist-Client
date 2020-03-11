1e9bb487cfa95460fb2dc72e869a0f79835d1e47
Rémi Sabatier <remi.sabatier31@gmail.com>
2020-03-11 18:48:19 +0100

    MAJ 11/03/20 v2

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Product } from '../world';
+
+declare var require; 
+const ProgressBar = require("progressbar.js");
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
+
 export class ProductComponent implements OnInit {
+  progressbar : any;
+  lastupdate: number;
+  product : Product;
 
   constructor() { }
 
-  product : Product;
     @Input()
     set prod(value:Product){
       this.product = value;
@@ -18,10 +24,26 @@ export class ProductComponent implements OnInit {
     }
 
   ngOnInit(): void {
+    this.progressbar = new ProgressBar.Line("#bar", {
+      strokeWidth: 100,
+      easing: 'easeInOut',
+      duration: 1400,
+      color: '#FFEA82',
+      trailColor: '#eee',
+      trailWidth: 1,
+      svgStyle: {width: '100%', height: '100%'}
+    });
+    
+    
   }
 
-  production(){
-    
+  startFabrication(){
+    if(this.product.quantite>=1){
+      this.progressbar.animate(1.0);
+      this.product.timeleft = this.product.vitesse;
+      this.lastupdate = Date.now();
+      this.progressbar.animated(1, { duration: this.product.vitesse});
+    }
   }
 
 }
